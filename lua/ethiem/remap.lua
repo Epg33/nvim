d94d81cc4589b26636ac01d6becd7d4b37614a97
EthiemGuerrero <ethiemalex03@gmail.com>
2023-07-22 18:48:53 -0500

    create folder and file

--- lua/ethiem/remap.lua
+++ lua/ethiem/remap.lua
@@ -3,4 +3,5 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<C-q>", ":q<CR>")
 vim.keymap.set("n", "<C-s>", ":w<CR>")
 vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>")
-
+vim.keymap.set("n", "<leader>f", ":e ")
+vim.keymap.set("n", "<leader>d", ":!mkdir ")
